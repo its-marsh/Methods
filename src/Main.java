176ac43911b5f70e34f9b3a3606adfe6781f0663
marsh <m.cowles21191@gmail.com>
2022-10-10 03:40:51 -0400

    second commit

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,15 @@ public class Main {
         calculateScore(true, 800, 5, 100);
         calculateScore(true, 10000, 8, 200);
     }
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
         if (gameOver){
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 2000;
             System.out.println("Your final score was " + finalScore);
+            return finalScore; // have to return some value, outside if-statement
         }
+        return -1; // -1 is invalid value by convention
     }
+
 }
